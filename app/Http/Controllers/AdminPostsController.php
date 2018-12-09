d6505b81e368df0bf48318f01c861609aca4a5e1
江明宸 <quzz96er43@mail.com>
2018-12-09 14:49:26 +0800

    Validation_練習2

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
-use App\Http\Requests;
+use App\Http\Requests\PostRequest;
 
 class AdminPostsController extends Controller
 {
